de1767c1e2bda6f12265502b9185af8eb01ddd0a
krikaz <pascal.bou@protonmail.com>
2020-03-15 23:44:44 +0100

    part 2, gig model

---
+++ Gigs/Gigs/Gig.swift
@@ -0,0 +1,15 @@
+//
+//  Gig.swift
+//  Gigs
+//
+//  Created by krikaz on 3/15/20.
+//  Copyright © 2020 thewire. All rights reserved.
+//
+
+import Foundation
+
+struct Gig {
+    var title: String
+    var description: String
+    var dueDate: Date
+}
