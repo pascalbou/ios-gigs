338958e86270c69f1d7dc325d84a0e513f13a5ef
krikaz <pascal.bou@protonmail.com>
2020-03-15 01:51:28 +0100

    removed spacing in code

--- Gigs/Gigs/LoginViewController.swift
+++ Gigs/Gigs/LoginViewController.swift
@@ -38,47 +38,47 @@ class LoginViewController: UIViewController {
         
         print("button tapped")
         
-         guard let gigController = gigController else { return }
+        guard let gigController = gigController else { return }
                  
-         if let username = usernameTextField.text,
-             !username.isEmpty,
-             let password = passwordTextField.text,
-             !password.isEmpty {
+        if let username = usernameTextField.text,
+            !username.isEmpty,
+            let password = passwordTextField.text,
+            !password.isEmpty {
              
-             print("it works 1")
-             let user = User(username: username, password: password)
+            print("it works 1")
+            let user = User(username: username, password: password)
              
-             if loginType == .signUp {
+            if loginType == .signUp {
                 print("it works 2")
                 gigController.signUp(with: user) { (error) in
                     if let error = error {
-                         NSLog("Error occured during sign up: \(error)")
-                     } else {
-                         print("test")
-                         DispatchQueue.main.async {
-                             let alertController = UIAlertController(title: "Sign Up Successful", message: "Now please log in.", preferredStyle: .alert)
-                             let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-                             alertController.addAction(alertAction)
-                             self.present(alertController, animated: true) {
-                                 self.loginType = .logIn
-                                 self.logInSegmentedControl.selectedSegmentIndex = 1
-                                 self.logInButton.setTitle("Log In", for: .normal)
-                             }
-                         }
-                     }
-                 }
-             } else {
-                 gigController.logIn(with: user) { (error) in
-                     if let error = error {
-                         NSLog("Error occured during sign up: \(error)")
-                     } else {
-                         DispatchQueue.main.async {
-                             self.dismiss(animated: true, completion: nil)
-                         }
-                     }
-                 }
-             }
-         }
+                        NSLog("Error occured during sign up: \(error)")
+                    } else {
+                        print("test")
+                        DispatchQueue.main.async {
+                            let alertController = UIAlertController(title: "Sign Up Successful", message: "Now please log in.", preferredStyle: .alert)
+                            let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                            alertController.addAction(alertAction)
+                            self.present(alertController, animated: true) {
+                                self.loginType = .logIn
+                                self.logInSegmentedControl.selectedSegmentIndex = 1
+                                self.logInButton.setTitle("Log In", for: .normal)
+                            }
+                        }
+                    }
+                }
+            } else {
+                gigController.logIn(with: user) { (error) in
+                    if let error = error {
+                        NSLog("Error occured during sign up: \(error)")
+                    } else {
+                        DispatchQueue.main.async {
+                            self.dismiss(animated: true, completion: nil)
+                        }
+                    }
+                }
+            }
+        }
     }
     
     
