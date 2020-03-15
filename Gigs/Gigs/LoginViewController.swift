0d762b0ea6790feaa0ebcea0f684aeeaa574adda
krikaz <pascal.bou@protonmail.com>
2020-03-15 02:00:56 +0100

    removed debug comments, sign up and login work

--- Gigs/Gigs/LoginViewController.swift
+++ Gigs/Gigs/LoginViewController.swift
@@ -35,26 +35,21 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
-        
-        print("button tapped")
-        
+                
         guard let gigController = gigController else { return }
-                 
+        
         if let username = usernameTextField.text,
             !username.isEmpty,
             let password = passwordTextField.text,
             !password.isEmpty {
              
-            print("it works 1")
             let user = User(username: username, password: password)
              
             if loginType == .signUp {
-                print("it works 2")
                 gigController.signUp(with: user) { (error) in
                     if let error = error {
                         NSLog("Error occured during sign up: \(error)")
                     } else {
-                        print("test")
                         DispatchQueue.main.async {
                             let alertController = UIAlertController(title: "Sign Up Successful", message: "Now please log in.", preferredStyle: .alert)
                             let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
