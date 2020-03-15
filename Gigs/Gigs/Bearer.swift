799f6baba2bcd5b1aa551b1f6cb0c5d1a830a12b
krikaz <pascal.bou@protonmail.com>
2020-03-15 01:03:36 +0100

    part 2

---
+++ Gigs/Gigs/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  Gigs
+//
+//  Created by krikaz on 3/15/20.
+//  Copyright © 2020 thewire. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    var token: String
+}
