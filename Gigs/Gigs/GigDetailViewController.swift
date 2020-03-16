8656942a0392d39df6889bbe9e67298f9ea43bc8
krikaz <pascal.bou@protonmail.com>
2020-03-16 01:06:54 +0100

    part 4, view controller implementation. Fetch all gigs works, add and save gig work but detailVC displays incorrectly

--- Gigs/Gigs/GigDetailViewController.swift
+++ Gigs/Gigs/GigDetailViewController.swift
@@ -14,14 +14,34 @@ class GigDetailViewController: UIViewController {
     @IBOutlet weak var dueDatePicker: UIDatePicker!
     @IBOutlet weak var descriptionTextView: UITextView!
     
+    var gigController: GigController?
+    var gig: Gig?
+    
     @IBAction func saveButton(_ sender: UIBarButtonItem) {
+        if let job = jobTextField.text, let description = descriptionTextView.text {
+            let newGig = Gig(title: job, description: description, dueDate: dueDatePicker.date)
+            gigController?.createGig(with: newGig, completion: { (result) in
+                if let _ = try? result.get() {
+                    DispatchQueue.main.async {
+                        self.updateViews()
+                    }
+                }
+            })
+        }
     }
     
+    private func updateViews() {
+        if let gig = gig {
+            jobTextField.text = gig.title
+            dueDatePicker.date = gig.dueDate
+            descriptionTextView.text = gig.description
+        } else {
+            title = "New Gig"
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
 
